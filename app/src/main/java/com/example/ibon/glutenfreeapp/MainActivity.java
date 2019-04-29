fe683b7b48ce21bdb4bda3c6294d4295490cc0cf
Ibon <ZzibonxX@gmail.com>
2019-04-29 20:07:10 +0200

    LOS BOTONES CAMBIAN DE FOTO AL PRESIONAR EN ELLOS Y AL SOLTAR

--- app/src/main/java/com/example/ibon/glutenfreeapp/MainActivity.java
+++ app/src/main/java/com/example/ibon/glutenfreeapp/MainActivity.java
@@ -1,26 +1,45 @@
 package com.example.ibon.glutenfreeapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView iv = findViewById(R.id.btnLista);
+        iv.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction()==MotionEvent.ACTION_DOWN){
+
+                    ImageView iv = findViewById(R.id.btnLista);
+                    iv.setImageResource(R.drawable.ic_mainhover);
+                }
+                else{
+                    if(event.getAction() == MotionEvent.ACTION_UP){
+                        ImageView iv = findViewById(R.id.btnLista);
+                        iv.setImageResource(R.drawable.ic_mainbtn);
+
+                        Intent intento = new Intent(v.getContext(),Lista.class);
+                        startActivity(intento);
+                        finish();
+                    }
+                }
+                return true;
+            }
+        });
     }
-
-
-
-    public void lista (View view){
-
-        Intent intento=new Intent(this,Lista.class);
-        startActivity(intento);
-        finish();
-    }
-
-
 }
